0ca6da60ffc2664eec303855237613dcaaedc473
gutpunch333 <gshields712@gmail.com>
2022-10-04 21:31:45 -0400

    animation commit

--- tut1.5/Assets/Scripts/catController.cs
+++ tut1.5/Assets/Scripts/catController.cs
@@ -5,57 +5,90 @@ using UnityEngine;
 
 
 
-public class soundmaybe : MonoBehaviour
+public class catController : MonoBehaviour
 {
 
-    public AudioClip musicClipOne;
-    public AudioClip musicClipTwo;
-    public AudioSource musicSource;
+   public AudioClip musicClipOne;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public AudioClip musicClipTwo;
 
-    // Update is called once per frame
-    void Update()
+public AudioSource musicSource;
+
+Animator anim;
+
+void Start()
+
 {
+
+  anim = GetComponent<Animator>();
+
+}
+
+// Update is called once per frame
+
+void Update()
+
+{
+
      if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.L))
+
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
 
-        
-   }
+    }
 }
